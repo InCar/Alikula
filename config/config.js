4ab29a92506fc73175e6301fa5882b114b4bc976
jiangli <hyacinth0509@163.com>
2015-10-29 11:06:53 +0800

    modify

--- config/config.js
+++ config/config.js
@@ -1,3 +1,7 @@
-/**
+
+module.exports = {
+    port: process.env.INCAR_PORT || 80,
+    logLevel: process.env.INCAR_LOG_LEVEL || 2 // 1:Error 2:Warning 3:Info 4:Detail
+};/**
  * Created by liz on 2015/10/29.
  */
--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,8 @@
 'use strict';
 
-var dis = require('../src/dispatcher');
-
 module.exports = function(app) {
-    app.post('/message/send/:sim/:cmd',dis.receiveMessageRequest);
-};
+
+
+}
 
 
