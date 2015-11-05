335a407d1adae64c2dfb57d185a4ae7d37ffb9f9
tianye <ytian@incardata.com.cn>
2015-11-05 10:53:56 +0800

    config

---
+++ config/config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by liz on 2015/10/29.
+ */
+module.exports = {
+    port: process.env.INCAR_PORT || 8080,
+    logLevel: process.env.INCAR_LOG_LEVEL || 2, // 1:Error 2:Warning 3:Info 4:Detail
+    aliyunCMSUrl: 'http://metrics.aliyuncs.com',
+    aliyunCMSVersion: '2015-04-20',
+    aliyunAccessKeyId: '6VXnxE3VeyLvGS57',
+    aliyunAccessKeySecret: 'JAOJwWTyCbrGTIYO2bRQapltCFLzOb'
+};
